8f20218025b73b689d385a1f21aa7a6f1f9190be
pmannuel <pmannuel@gmail.com>
2017-01-30 11:59:09 -0500

    Updated ifYouDontMind Assignment

--- JavaScript/ifYouDontMind.js
+++ JavaScript/ifYouDontMind.js
@@ -1,47 +1,75 @@
-// function timeteller(hour, min, period) {
-//
-//     if (min > 30) {
-//         console.log("It's almost ");
-//     } else {
-//         console.log("It's just after ");
-//     }
-//     console.log(hour);
-//     if (period == "AM") {
-//         console.log(" in the morning");
-//     } else {
-//         console.log(" in the evening");
-//     }
-// }
-
-//////////////////
-//REAL TIME DATE//
-//////////////////
-
-//test variables - commented out when not in use
-// var hour = 0;
-// var min = 0;
-
 var d = new Date();
-var hour = d.getHours(); //NOT SURE if hours with single digit are given as 1 or 01, too lazy to find out :p
+var hour = d.getHours();
 var min = d.getMinutes();
-// console.log(hour + ":" + min);
+console.log(hour + ":" + min);
 
-if (min > 30) {
-    var txt = "It's almost ";
-} else {
+//If You Don't Mind Assignment.
+//Assuming input is in military time format.
+
+var hour = 20;
+var min = 30;
+
+///////////////////////////////////////////////////////////
+//DEFINING Description Whether it's quarter or half past //
+///////////////////////////////////////////////////////////
+
+while(min <= 30){
+if( min === 0){
+  var txt = "It's ";
+}
+  else if(min == 5) {
+    var txt = "It's 5 after ";
+}
+else if (min == 15) {
+    var txt = "It's quarter after ";
+}
+else if (min == 30) {
+    var txt = "It's half past ";
+}
+else{
     var txt = "It's just after ";
 }
-
-if (hour < 12) {
-    var period = " in the morning";
-} else if (hour == 12) {
-    var period = " at noon";
-} else if (hour == 0) {
-    var period = " at midnight";
-} else {
-    var period = " in the evening";
+  break;
 }
 
-var hour = (hour - 12);
+while (min > 30) {
+    var txt = "It's almost ";
+    break;
+}
+
+/////////////////////////////////////////////////////////////
+//DEFINING PERIOD. Whether it's noon, afternoon, or evening//
+/////////////////////////////////////////////////////////////
+
+while(hour < 17){ //if hour is between 0 AM and 4 PM
+    if (hour == 12) {
+        var period = " at noon";
+    }
+    else if (hour > 12 && hour < 17) {
+        var period = " in the afternoon";
+    }
+    else if (hour === 0) {
+        var period = " at midnight";
+    }
+    else{
+        var period = " in the morning";
+    }
+    break;
+}
+
+while(hour >= 17){ //hour is between 4 PM and 11:59 PM
+    if (hour >= 17 && hour <=19){
+        var period = " in the evening";
+    }
+    else{
+        var period = " at night";
+    }
+    break;
+}
+
+
+if (hour > 12) {
+    hour = (hour - 12);
+}
 
 console.log(txt + hour + period);
