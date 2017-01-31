e4bd8a51301e8c41be08698e1c7d12d1ffd55dd3
pmannuel <pmannuel@gmail.com>
2017-01-31 17:21:42 -0500

    Completed jQuery module. Started advanced jQuery module.

--- JavaScript/10000reward.js
+++ JavaScript/10000reward.js
@@ -1,10 +1,10 @@
 var money = 0.01;
 
-//How much reward after 30 days?
-for (var d = 2; d <= 30; d++) {
-    money = money * 2;
-}
-console.log("He was rewarded $" + money);
+// //How much reward after 30 days?
+// for (var d = 2; d <= 30; d++) {
+//     money = money * 2;
+// }
+// console.log("He was rewarded $" + money);
 
 //How many days to earn $10,000?
 for (var d = 2; d <= 30; d++) {
@@ -12,6 +12,6 @@ for (var d = 2; d <= 30; d++) {
         console.log("It took " + d + " days");
         break;
     } else {
-        money = money * 2;
+        money *=2;
     }
 }
